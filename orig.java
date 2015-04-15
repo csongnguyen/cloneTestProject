3a20e65e2bf0f2ac119385eabc532acb4384cd57
connie <csnguyen@ucdavis.edu>
2015-04-15 16:38:01 -0700

    forgot to save orig.java from last commit

--- orig.java
+++ orig.java
@@ -31,6 +31,16 @@ public class CharSeqIt {
 		 this.iter = -1;
 		 1+1 = 2;
 		 3 + 4 = 7;
+		 5*20 = 100;
+		 int tango = 1;
+		 int b = -1;
+		 int a= b;
+		 int d = 0;
+		 int c = d;
+		 int t = tango;
+		 6 == 1000 * 64;
+		 50 == 234 / 543;
+
 	 }
 
 }
